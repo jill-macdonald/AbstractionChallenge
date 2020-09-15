339971bad537749ea9c08f59c3c5fa51fb6e886f
jill-macdonald <jillmacdonald23>
2020-09-15 16:39:01 +0100

    remove methods for SearchTree updated.

--- src/com/mcsd/Main.java
+++ src/com/mcsd/Main.java
@@ -8,8 +8,8 @@ public class Main {
         tree.traverse(tree.getRoot()); // should be empty
 
         //create a string data array to avoid typing loads of addItem instructions
-        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
-        //String stringData = "5 7 3 9 8 2 1 0 4 6";
+        //String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
 
         String [] data = stringData.split(" "); //add items into string array from line 11
         //(" ") is the limiter, separates each key string
@@ -17,9 +17,9 @@ public class Main {
             tree.addItem(new Node(s)); //add capital city
         }
 
-        tree.traverse(tree.getRoot()); //should now pass the items
+        //tree.traverse(tree.getRoot()); //should now pass the items
 
- /*       tree.traverse(tree.getRoot());
+        tree.traverse(tree.getRoot());
         tree.removeItem(new Node("3"));
         tree.traverse(tree.getRoot());
 
@@ -40,7 +40,7 @@ public class Main {
         tree.removeItem(tree.getRoot()); //last entry
         tree.traverse(tree.getRoot());
         tree.removeItem(tree.getRoot());
-        tree.traverse(tree.getRoot());*/
+        tree.traverse(tree.getRoot());
 
 
 
